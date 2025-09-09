aba3f2d285181220bb6ae34d448933dfabc7492f
Lucas Nunzi <lucas.nunzi@life5.com>
2025-09-09 13:16:05 +0000

    phpactor more keys

--- lua/user/plugins/phpactor.lua
+++ lua/user/plugins/phpactor.lua
@@ -3,5 +3,11 @@ return {
   branch = "master",
   ft = "php", -- lo carga solo en buffers PHP
   build = "composer install --no-dev -o", -- instala dependencias
+  config = function()
+    -- Opcional: mapeos cómodos
+    vim.keymap.set("n", "<leader>pm", "<cmd>PhpactorContextMenu<CR>", { desc = "Phpactor context menu" })
+    vim.keymap.set("n", "<leader>pi", "<cmd>PhpactorImportMissingClasses<CR>", { desc = "Phpactor import missing classes" })
+    vim.keymap.set("n", "<leader>pn", "<cmd>PhpactorClassExpand<CR>", { desc = "Phpactor expand class" })
+  end,
 }
 
