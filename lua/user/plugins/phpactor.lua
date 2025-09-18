a5ec3f7a861b94020ecb987b874d4debfd5af9c1
Lucas Nunzi <lucas.nunzi@life5.com>
2025-09-18 11:53:19 +0000

    phpactor keys

--- lua/user/plugins/phpactor.lua
+++ lua/user/plugins/phpactor.lua
@@ -1,15 +1,18 @@
 return {
   "phpactor/phpactor",
   branch = "master",
-  ft = "php", -- lo carga solo en buffers PHP
+  ft = "php",                             -- lo carga solo en buffers PHP
   build = "composer install --no-dev -o", -- instala dependencias
   dependencies = {
     "neovim/nvim-lspconfig",
   },
   config = function()
     -- Opcional: mapeos cómodos
-    vim.keymap.set("n", "<leader>pm", "<cmd>PhpactorContextMenu<CR>", { desc = "Phpactor context menu" })
+    vim.keymap.set("n", "<leader>mm", "<cmd>PhpactorContextMenu<CR>", { desc = "Phpactor context menu" })
     vim.keymap.set("n", "<leader>pi", "<cmd>PhpactorImportMissingClasses<CR>", { desc = "Phpactor import missing classes" })
-    vim.keymap.set("n", "<leader>pn", "<cmd>PhpactorClassExpand<CR>", { desc = "Phpactor expand class" })
+    vim.keymap.set("n", "<leader>pc", "<cmd>PhpactorClassExpand<CR>", { desc = "Phpactor expand class" })
+    vim.keymap.set("n", "<leader>pn", "<cmd>PhpactorClassNew<CR>", { desc = "Phpactor new class" })
+    vim.keymap.set("n", "<leader>pr", "<cmd>PhpactorRenameVariable<CR>", { desc = "Phpactor rename variable" })
+    vim.keymap.set("n", "<leader>pm", "<cmd>PhpactorMoveFile<CR>",  { desc = "Phpactor move file" })
   end,
 }
