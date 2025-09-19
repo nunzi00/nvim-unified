abb98f679a8d7848bb4b29fbaea615fe431b7049
Lucas Nunzi <lucas.nunzi@life5.com>
2025-09-19 11:02:14 +0000

    quitamos vendor de las exclusiones para que intelephense no se queje por no encontrar las clases

--- lua/user/plugins/lsp.lua
+++ lua/user/plugins/lsp.lua
@@ -72,7 +72,6 @@ return {
               exclude = {
                 "**/.git/**",
                 "**/node_modules/**",
-                "**/vendor/**",
                 "**/var/**",
               },
             },
