4222a5c571b9b95f797954d47db40b47f06444f1
Lucas Nunzi <lucas.nunzi@life5.com>
2025-11-07 07:31:59 +0000

    Revert "fix(lsp): lsp keymaps not working after update"
    This reverts commit d475e7f8c00c59d63fbb6d1335a53de45aea7ab7.

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -124,4 +124,20 @@ keymap("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
 
 
 
+-- Plugins
+-- LSP
+keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+keymap("n", "<leader>d", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+keymap("n", "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+keymap("n", "<leader>lf", "<cmd>lua vim.lsp.buf.format({ async = true })<CR>", opts)
+keymap("n", "<leader>li", "<cmd>LspInfo<CR>", opts)
+keymap("n", "[d", "<cmd>lua vim.diagnostic.goto_next({ buffer = 0 })<CR>", opts)
+keymap("n", "]d", "<cmd>lua vim.diagnostic.goto_prev({ buffer = 0 })<CR>", opts)
+keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+keymap("n", "<leader>sh", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
 
+keymap("n", "<leader>li", "<cmd>LspInfo<CR>", opts)
